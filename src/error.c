88784c48fb9ba7c46e5236ba78ef9437a4776a86
Suzuki Go <puyogo.suzuki@gmail.com>
2022-04-27 21:54:39 +0900

    [FIX] Visual C++ Compilation Error

--- src/error.c
+++ src/error.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-
+#if !defined(_MSC_VER)
 void
 errorOut(char * errorLevel, char * moduleName, char * message) {
 	fprintf(stderr, "[%s] %s: %s\n", errorLevel, moduleName, message);
 }
+#endif
