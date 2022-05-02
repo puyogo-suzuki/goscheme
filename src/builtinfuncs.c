b5fc1e0491446b4a08b54009c7ba18b7cfcf59c6
Go Suzuki <puyogo.suzuki@gmail.com>
2022-05-02 17:26:04 +0900

    [IMPROVE] make more less code for arthemetic code.

--- src/builtinfuncs.c
+++ src/builtinfuncs.c
@@ -101,129 +101,73 @@ ONE_ARGUMENT_FUNC(builtin_cddddr, "cddddr", CHKERROR(schemeObject_cddddr(arg0, &
     CHKERROR(gc_deref_schemeObject(prev_cdr)) \
 }
 
-gserror_t
-builtin_additive(machine_t * self, environment_t * env, schemeObject_t * val, evaluationResult_t * out) {
-	int32_t retval = 0;
-    if(!schemeObject_isList(val)) {
-		errorOut("ERROR", "+", "proper list.");
-		return ERR_EVAL_INVALID_OBJECT_TYPE;
-    }
-    if(val != SCHEME_OBJECT_NILL) CHKERROR(gc_ref(&(val->gcInfo)))
-    schemeObject_t * car, * cdr = val, * carres;
-    while(cdr != SCHEME_OBJECT_NILL) {
-        READ_HEAD(self, env, car, carres, cdr)
-        if(carres->kind != SCHEME_OBJECT_NUMBER) {
-            errorOut("ERROR", "+", "Not number coming.");
-            CHKERROR(gc_deref_schemeObject(cdr))
-            CHKERROR(gc_deref_schemeObject(carres))
-            return ERR_EVAL_INVALID_OBJECT_TYPE;
-        }
-        retval += carres->value.numValue;
-        CHKERROR(gc_deref_schemeObject(carres))
-    }
-    schemeObject_t * outobj = (schemeObject_t *)reallocarray(NULL, 1, sizeof(schemeObject_t));
-    if(outobj == NULL) return ERR_OUT_OF_MEMORY;
-    CHKERROR(schemeObject_new_number(outobj, retval))
-    CHKERROR(gc_ref(&(outobj->gcInfo)))
-    out->kind = EVALUATIONRESULT_EVALUATED;
-    out->value.evaluatedValue = outobj;
-    return ERR_SUCCESS;
-}
+#define ARTHMETIC_LEAST0_FUNC(name, name_str, retval_init, retval_final, updater) gserror_t \
+name(machine_t * self, environment_t * env, schemeObject_t * val, evaluationResult_t * out) { \
+	retval_init \
+    if(!schemeObject_isList(val)) { \
+		errorOut("ERROR", name_str, "proper list."); \
+		return ERR_EVAL_INVALID_OBJECT_TYPE; \
+    } \
+    if(val != SCHEME_OBJECT_NILL) CHKERROR(gc_ref(&(val->gcInfo))) \
+    schemeObject_t * car, * cdr = val, * carres; \
+    while(cdr != SCHEME_OBJECT_NILL) { \
+        READ_HEAD(self, env, car, carres, cdr) \
+        if(carres->kind != SCHEME_OBJECT_NUMBER) { \
+            errorOut("ERROR", "+", "Not number coming."); \
+            CHKERROR(gc_deref_schemeObject(cdr)) \
+            CHKERROR(gc_deref_schemeObject(carres)) \
+            return ERR_EVAL_INVALID_OBJECT_TYPE; \
+        } \
+        updater \
+        CHKERROR(gc_deref_schemeObject(carres)) \
+    } \
+    schemeObject_t * outobj = (schemeObject_t *)reallocarray(NULL, 1, sizeof(schemeObject_t)); \
+    if(outobj == NULL) return ERR_OUT_OF_MEMORY; \
+    CHKERROR(retval_final) \
+    CHKERROR(gc_ref(&(outobj->gcInfo))) \
+    out->kind = EVALUATIONRESULT_EVALUATED; \
+    out->value.evaluatedValue = outobj; \
+    return ERR_SUCCESS; \
+} \
 
-gserror_t
-builtin_subtract(machine_t * self, environment_t * env, schemeObject_t * val, evaluationResult_t * out) {
-	int32_t retval = 0;
-    if(!schemeObject_isList(val)) {
-		errorOut("ERROR", "-", "proper list.");
-		return ERR_EVAL_INVALID_OBJECT_TYPE;
-    }
-    if(val != SCHEME_OBJECT_NILL) CHKERROR(gc_ref(&(val->gcInfo)))
-    schemeObject_t * car, * cdr = val, * carres;
-    READ_HEAD(self, env, car, carres, cdr)
-    if(carres->kind != SCHEME_OBJECT_NUMBER) goto L_NOT_COMING_NUMBER;
-    retval = cdr == SCHEME_OBJECT_NILL ? -carres->value.numValue : carres->value.numValue;
-    while(cdr != SCHEME_OBJECT_NILL) {
-        READ_HEAD(self, env, car, carres, cdr)
-        if(carres->kind != SCHEME_OBJECT_NUMBER) goto L_NOT_COMING_NUMBER;
-        retval -= carres->value.numValue;
-        CHKERROR(gc_deref_schemeObject(carres))
-    }
-    schemeObject_t * outobj = (schemeObject_t *)reallocarray(NULL, 1, sizeof(schemeObject_t));
-    if(outobj == NULL) return ERR_OUT_OF_MEMORY;
-    CHKERROR(schemeObject_new_number(outobj, retval))
-    CHKERROR(gc_ref(&(outobj->gcInfo)))
-    out->kind = EVALUATIONRESULT_EVALUATED;
-    out->value.evaluatedValue = outobj;
-    return ERR_SUCCESS;
+ARTHMETIC_LEAST0_FUNC(builtin_additive, "+", int32_t retval = 0;, schemeObject_new_number(outobj, retval), retval += carres->value.numValue;)
+ARTHMETIC_LEAST0_FUNC(builtin_multiplication, "*", int32_t retval = 1;, schemeObject_new_number(outobj, retval), retval *= carres->value.numValue;)
 
-L_NOT_COMING_NUMBER:
-    errorOut("ERROR", "-", "Not number coming.");
-    CHKERROR(gc_deref_schemeObject(cdr))
-    CHKERROR(gc_deref_schemeObject(carres))
-    return ERR_EVAL_INVALID_OBJECT_TYPE;
-}
+#define ARTHMETIC_LEAST1_FUNC(name, name_str, retval_init, init2, retval_final, updater) gserror_t \
+name(machine_t * self, environment_t * env, schemeObject_t * val, evaluationResult_t * out) { \
+	retval_init \
+    if(!schemeObject_isList(val)) { \
+		errorOut("ERROR", name_str, "proper list."); \
+		return ERR_EVAL_INVALID_OBJECT_TYPE; \
+    } \
+    if(val != SCHEME_OBJECT_NILL) CHKERROR(gc_ref(&(val->gcInfo))) \
+    schemeObject_t * car, * cdr = val, * carres; \
+    READ_HEAD(self, env, car, carres, cdr) \
+    if(carres->kind != SCHEME_OBJECT_NUMBER) goto L_NOT_COMING_NUMBER; \
+    init2 \
+    while(cdr != SCHEME_OBJECT_NILL) { \
+        READ_HEAD(self, env, car, carres, cdr) \
+        if(carres->kind != SCHEME_OBJECT_NUMBER) goto L_NOT_COMING_NUMBER; \
+        retval -= carres->value.numValue; \
+        CHKERROR(gc_deref_schemeObject(carres)) \
+    } \
+    schemeObject_t * outobj = (schemeObject_t *)reallocarray(NULL, 1, sizeof(schemeObject_t)); \
+    if(outobj == NULL) return ERR_OUT_OF_MEMORY; \
+    CHKERROR(retval_final) \
+    CHKERROR(gc_ref(&(outobj->gcInfo))) \
+    out->kind = EVALUATIONRESULT_EVALUATED; \
+    out->value.evaluatedValue = outobj; \
+    return ERR_SUCCESS; \
+\
+L_NOT_COMING_NUMBER: \
+    errorOut("ERROR", "-", "Not number coming."); \
+    CHKERROR(gc_deref_schemeObject(cdr)) \
+    CHKERROR(gc_deref_schemeObject(carres)) \
+    return ERR_EVAL_INVALID_OBJECT_TYPE; \
+} \
 
-gserror_t
-builtin_multiplication(machine_t * self, environment_t * env, schemeObject_t * val, evaluationResult_t * out) {
-	int32_t retval = 1;
-    if(!schemeObject_isList(val)) {
-		errorOut("ERROR", "*", "proper list.");
-		return ERR_EVAL_INVALID_OBJECT_TYPE;
-    }
-    if(val != SCHEME_OBJECT_NILL) CHKERROR(gc_ref(&(val->gcInfo)))
-    schemeObject_t * car, * cdr = val, * carres;
-    while(cdr != SCHEME_OBJECT_NILL) {
-        READ_HEAD(self, env, car, carres, cdr)
-        if(carres->kind != SCHEME_OBJECT_NUMBER) {
-            errorOut("ERROR", "*", "Not number coming.");
-            CHKERROR(gc_deref_schemeObject(cdr))
-            CHKERROR(gc_deref_schemeObject(carres))
-            return ERR_EVAL_INVALID_OBJECT_TYPE;
-        }
-        retval *= carres->value.numValue;
-        CHKERROR(gc_deref_schemeObject(carres))
-    }
-    schemeObject_t * outobj = (schemeObject_t *)reallocarray(NULL, 1, sizeof(schemeObject_t));
-    if(outobj == NULL) return ERR_OUT_OF_MEMORY;
-    CHKERROR(schemeObject_new_number(outobj, retval))
-    CHKERROR(gc_ref(&(outobj->gcInfo)))
-    out->kind = EVALUATIONRESULT_EVALUATED;
-    out->value.evaluatedValue = outobj;
-    return ERR_SUCCESS;
-}
-
-gserror_t
-builtin_division(machine_t * self, environment_t * env, schemeObject_t * val, evaluationResult_t * out) {
-	int32_t retval = 0;
-    if(!schemeObject_isList(val)) {
-		errorOut("ERROR", "/", "proper list.");
-		return ERR_EVAL_INVALID_OBJECT_TYPE;
-    }
-    if(val != SCHEME_OBJECT_NILL) CHKERROR(gc_ref(&(val->gcInfo)))
-    schemeObject_t * car, * cdr = val, * carres;
-    READ_HEAD(self, env, car, carres, cdr)
-    if(car->kind != SCHEME_OBJECT_NUMBER) goto L_NOT_COMING_NUMBER;
-    retval = cdr == SCHEME_OBJECT_NILL ? 0 : carres->value.numValue; // TODO: Rational Value.
-    while(cdr != SCHEME_OBJECT_NILL) {
-        READ_HEAD(self, env, car, carres, cdr)
-        if(carres->kind != SCHEME_OBJECT_NUMBER) goto L_NOT_COMING_NUMBER;
-        retval /= carres->value.numValue;
-        CHKERROR(gc_deref_schemeObject(carres))
-    }
-    schemeObject_t * outobj = (schemeObject_t *)reallocarray(NULL, 1, sizeof(schemeObject_t));
-    if(outobj == NULL) return ERR_OUT_OF_MEMORY;
-    CHKERROR(schemeObject_new_number(outobj, retval))
-    CHKERROR(gc_ref(&(outobj->gcInfo)))
-    out->kind = EVALUATIONRESULT_EVALUATED;
-    out->value.evaluatedValue = outobj;
-    return ERR_SUCCESS;
-
-L_NOT_COMING_NUMBER:
-    errorOut("ERROR", "/", "Not number coming.");
-    CHKERROR(gc_deref_schemeObject(cdr))
-    CHKERROR(gc_deref_schemeObject(carres))
-    return ERR_EVAL_INVALID_OBJECT_TYPE;
-}
+ARTHMETIC_LEAST1_FUNC(builtin_subtract, "-", int32_t retval = 0;, retval = cdr == SCHEME_OBJECT_NILL ? -carres->value.numValue : carres->value.numValue;, schemeObject_new_number(outobj, retval), retval -= carres->value.numValue;)
+ARTHMETIC_LEAST1_FUNC(builtin_division, "/", int32_t retval = 0;, retval = cdr == SCHEME_OBJECT_NILL ? 0 : carres->value.numValue;, schemeObject_new_number(outobj, retval), retval /= carres->value.numValue;)
 
 #define PRED_FUNC(funcname, funcname_str, pred)  ONE_ARGUMENT_FUNC(funcname, funcname_str, { \
     outobj = (pred) ? &predefined_t : &predefined_f; \
